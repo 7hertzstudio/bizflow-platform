01e01bcb73d5b20fe515a6a3766344fed3e04b77
JayXD <hello@jayxd.com>
2026-02-09 11:45:18 +0500

    fix(resources): clean up corrupted page files
    Uses write_file to properly overwrite pages for Categories and Products, removing terminal escape character corruption.

--- app/Filament/Resources/Categories/Pages/EditCategory.php
+++ app/Filament/Resources/Categories/Pages/EditCategory.php
@@ -1,5 +1,11 @@
 <?php
+
 namespace App\Filament\Resources\Categories\Pages;
+
 use App\Filament\Resources\Categories\CategoryResource;
-use Filament\Resources\PagesditRecord;
-class EditCategory extends EditRecord { protected static string $resource = CategoryResource::class; }
+use Filament\Resources\Pages\EditRecord;
+
+class EditCategory extends EditRecord
+{
+    protected static string $resource = CategoryResource::class;
+}
